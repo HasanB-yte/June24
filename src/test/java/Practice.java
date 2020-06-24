1438ada8c85e9ec96b9f871a99c04779a8938e47
Roman86-art <special4u86@ukr.net>
2020-06-24 15:38:05 -0500

    We will  do  it

--- src/test/java/Practice.java
+++ src/test/java/Practice.java
@@ -2,6 +2,8 @@ public class Practice {
 
     public static void main(String[] args) {
         System.out.println("hello Hasan world");
+        System.out.println("hello Hasan world1");
+
     }
 
 }
