5c218a7e7be9c5b6567d1a78c818a7fa76313768
Silond <silond@gmail.com>
2020-06-24 15:46:26 -0500

    new class

--- src/test/java/Practice.java
+++ src/test/java/Practice.java
@@ -1,7 +1,8 @@
 public class Practice {
 
     public static void main(String[] args) {
-        System.out.println("hello Hasan world")
+        System.out.println("hello Hasan world");
+        System.out.println("hello Roman world");
     
     }
 
